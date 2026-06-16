260e23290fdd29104af9c9c92686901855f9b683
devVargas <alissonvargas97@gmail.com>
2026-06-16 13:07:04 -0300

    feat: apps accounts

---
+++ backend/apps/accounts/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
+
+from apps.accounts.views import (
+    LoginView,
+    MeView,
+    ProfessorRegistrationView,
+    StudentRegistrationView,
+)
+
+urlpatterns = [
+    path("login/", LoginView.as_view()),
+    path("refresh/", TokenRefreshView.as_view()),
+    path("me/", MeView.as_view(), name="me"),
+    path("register/student/", StudentRegistrationView.as_view()),
+    path("register/professor/", ProfessorRegistrationView.as_view()),
+]
